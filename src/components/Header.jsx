5d4fcf3ceb33e60c16c61d86a9192f37bd8892b2
NguyenThuan <thuans2nd@gmail.com>
2022-07-25 14:47:38 +0700

    Revert "Update dropmenu"
    This reverts commit 9045d5a8ad5eadf0b85a93b925e999f3762be932.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,7 @@
 import React, { useRef, useEffect } from "react";
 import { Link, useLocation } from "react-router-dom";
 import logo from "../assets/images/logo.jpg";
-import { useDetectOutsideClick } from "../utils/common";
+
 const mainNav = [
   {
     display: "Trang chủ",
@@ -49,75 +49,53 @@ const Header = () => {
 
   const menuToggle = () => menuLeft.current.classList.toggle("active");
 
-  const dropdownRef = useRef(null);
-  const [isActive, setIsActive] = useDetectOutsideClick(dropdownRef, false);
-  const onClick = () => setIsActive(!isActive);
   return (
     <div className="header" ref={headerRef}>
-      <>
-        <div className="container">
-          <div className="header__logo">
-            <Link to="/">
-              <img src={logo} alt="" />
-            </Link>
-          </div>
-          <div className="header__menu">
-            <div className="header__menu__mobile-toggle" onClick={menuToggle}>
-              <i className="bx bx-menu-alt-left"></i>
+        <>
+          <div className="container">
+            <div className="header__logo">
+              <Link to="/">
+                <img src={logo} alt="" />
+              </Link>
             </div>
-            <div className="header__menu__left" ref={menuLeft}>
-              <div className="header__menu__left__close" onClick={menuToggle}>
-                <i className="bx bx-chevron-left"></i>
+            <div className="header__menu">
+              <div className="header__menu__mobile-toggle" onClick={menuToggle}>
+                <i className="bx bx-menu-alt-left"></i>
               </div>
-              {mainNav.map((item, index) => (
-                <div
-                  key={index}
-                  className={`header__menu__item header__menu__left__item ${
-                    index === activeNav ? "active" : ""
-                  }`}
-                  onClick={menuToggle}
-                >
-                  <Link to={item.path}>
-                    <span>{item.display}</span>
+              <div className="header__menu__left" ref={menuLeft}>
+                <div className="header__menu__left__close" onClick={menuToggle}>
+                  <i className="bx bx-chevron-left"></i>
+                </div>
+                {mainNav.map((item, index) => (
+                  <div
+                    key={index}
+                    className={`header__menu__item header__menu__left__item ${
+                      index === activeNav ? "active" : ""
+                    }`}
+                    onClick={menuToggle}
+                  >
+                    <Link to={item.path}>
+                      <span>{item.display}</span>
+                    </Link>
+                  </div>
+                ))}
+              </div>
+              <div className="header__menu__right">
+                <div className="header__menu__item header__menu__right__item">
+                  <i className="bx bx-search"></i>
+                </div>
+                <div className="header__menu__item header__menu__right__item">
+                  <Link to="/cart">
+                    <i className="bx bx-shopping-bag"></i>
                   </Link>
                 </div>
-              ))}
-            </div>
-            <div className="header__menu__right">
-              <div className="header__menu__item header__menu__right__item">
-                <i className="bx bx-search"></i>
+                <div className="header__menu__item header__menu__right__item">
+                  <i className="bx bx-user"></i>
+                </div>
               </div>
-              <div className="header__menu__item header__menu__right__item">
-                <Link to="/cart">
-                  <i className="bx bx-shopping-bag"></i>
-                </Link>
-              </div>
-              <div
-                onClick={onClick}
-                className="header__menu__item header__menu__right__item"
-              >
-                <i className="bx bx-user"></i>
-              </div>
-              <nav
-                ref={dropdownRef}
-                className={`menu ${isActive ? "active" : "inactive"}`}
-              >
-                <ul>
-                  <li>
-                    <a href="#">Messages</a>
-                  </li>
-                  <li>
-                    <a href="#">Trips</a>
-                  </li>
-                  <li>
-                    <a href="#">Saved</a>
-                  </li>
-                </ul>
-              </nav>
             </div>
           </div>
-        </div>
-      </>
+        </>
     </div>
   );
 };
